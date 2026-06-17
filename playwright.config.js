558bdd0a9ec2f5f645e44be27754453ddeeacf72
Emiliano Fausto <emiliano.fausto@gmail.com>
2026-06-17 23:20:12 +0200

    play

--- playwright.config.js
+++ playwright.config.js
@@ -1,19 +1,15 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
-  // Path to the directory containing end-to-end test files
-  testDir: './tests', 
+  testDir: './frontend/tests', 
   fullyParallel: true,
-  // Fail the build on CI if you accidentally leave test.only in the source code
   forbidOnly: !!process.env.CI,
-  // Retry failing tests on CI environments to mitigate flakiness
   retries: process.env.CI ? 2 : 0,
-  // Opt out of parallel tests on CI to preserve system resources
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   
   use: {
-    baseURL: 'http://127.0.0.1:3000', 
+    baseURL: 'http://127.0.0.1:5500', 
     trace: 'on-first-retry',
   },
 
@@ -23,11 +19,4 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     }
   ],
-
-  webServer: {
-    command: 'npm run dev', 
-    url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 60000,
-  },
 });
